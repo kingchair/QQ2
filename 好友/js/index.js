2ec9c6d3ecaab2c966b6bdc5c93fe10b82b37157
kingchair <41765954+kingchair@users.noreply.github.com>
2018-08-06 11:13:51 +0800

    Add files via upload

---
+++ 好友/js/index.js
@@ -0,0 +1,10 @@
+$(function(){
+      $("#care").click( function () {
+        $("#care_chat").toggle(200);
+      })
+}
+)
+// $("span").click(function(){
+//   var src = "img/arrow.svg";    //新图片地址
+//   $(this).find("img").attr("img/down.svg",src);    //更换图片地址
+// });
---
+++ 首页/js/index.js
@@ -0,0 +1,6 @@
+$(function(){
+      $("#title_right_add").click( function () {
+        $("#add_menu").toggle(200);
+      })
+}
+)
